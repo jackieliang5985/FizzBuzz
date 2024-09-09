e8617613317ccbc679309e61f5b19c814e3a667a
jackieliang5985 <dinowoolly79@gmail.com>
2024-09-09 14:13:21 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples());
+    }
+    public static int multiples() {
+        int i = 0;
+        for (int num = 1; num < 1000; num++) {
+            boolean divisibleBy3 = num % 3 == 0;
+            boolean divisibleBy5 = num % 5 == 0;
+            if (divisibleBy3 || divisibleBy5) {
+                i++;
+            }
+        }
+        return i;
+    }
 }
